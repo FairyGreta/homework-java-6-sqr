08f93a88a7cb026a86750ae8847db2e55233011c
Kristina Artyushechkina <104067981+FairyGreta@users.noreply.github.com>
2022-06-03 10:45:08 +0500

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/homework/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/homework/SQRServiceTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class SQRServiceTest {
     @ParameterizedTest
     @CsvFileSource(files="src/test/resources/number.csv")
-    public void testCountSqrInTheRangeFrom200To300(int expected, int firstNum, int lastNum, int lowerLimit, int upperLimit) {
+    public void testCountSqrInTheRangeFrom200To300(int expected, int lowerLimit, int upperLimit) {
 
         SQRService service = new SQRService();
         int actual = service.calculateSqr(lowerLimit, upperLimit);
